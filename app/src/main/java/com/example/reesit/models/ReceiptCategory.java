8ccc0ca174fb85755ea6cfd95c73a60377a5af7b
Faruq Yusuff <faruqg@fb.com>
2022-07-03 19:50:45 -0700

    added new models for tags and categories, removed exceptions subpackage

---
+++ app/src/main/java/com/example/reesit/models/ReceiptCategory.java
@@ -0,0 +1,2 @@
+package com.example.reesit.models;public class ReceiptCategory {
+}
--- app/src/main/java/com/example/reesit/utils/AddReceiptCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.reesit.utils;
-
-import com.example.reesit.models.Receipt;
-import com.parse.ParseException;
-
-public interface AddReceiptCallback {
-    public void done(Receipt receipt, ParseException e);
-}
