d67349a0de7d07881b2a548d614bc88669d17da7
Faruq Yusuff <faruqg@fb.com>
2022-07-15 16:04:49 -0700

    started working on issue #33

--- app/src/main/java/com/example/reesit/fragments/ReceiptDetailsFragment.java
+++ app/src/main/java/com/example/reesit/fragments/ReceiptDetailsFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.activity.OnBackPressedCallback;
@@ -21,7 +22,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.reesit.R;
 import com.example.reesit.activities.ReceiptDetailsActivity;
 import com.example.reesit.databinding.FragmentReceiptDetailsBinding;
@@ -180,6 +187,19 @@ public class ReceiptDetailsFragment extends Fragment {
                 dialogBuilder.show();
                 return true;
             case R.id.share_receipt_menu_item:
+                Glide.with(requireContext()).load(receipt.getReceiptImage()).listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        Toast.makeText(requireContext(), R.string.share_receipt_load_receipt_image_error, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                        return false;
+                    }
+                });
                 return true;
             case R.id.download_receipt_menu_item:
                 return true;
