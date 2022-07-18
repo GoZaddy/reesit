c84ac831af18ae2b63bf0b62e23c7a425333acc7
Faruq Yusuff <faruqg@fb.com>
2022-07-18 11:59:27 -0700

    deleted unused method and added new method: rotateBitmapWithExif

--- app/src/main/java/com/example/reesit/utils/BitmapUtils.java
+++ app/src/main/java/com/example/reesit/utils/BitmapUtils.java
@@ -5,6 +5,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 
+import androidx.annotation.NonNull;
+
 import java.io.IOException;
 
 public class BitmapUtils {
@@ -21,6 +23,23 @@ public class BitmapUtils {
     {
         float factor = height / (float) b.getHeight();
         return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factor), height, true);
+    }
+
+    public static Bitmap rotateBitmapWithExif(@NonNull Bitmap sourceBitmap, @NonNull ExifInterface exifInterface){
+        String orientString = exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION);
+
+        System.out.println("orientString: "+orientString);
+
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle = 0;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) rotationAngle = 90;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_180) rotationAngle = 180;
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_270) rotationAngle = 270;
+        // Rotate Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) sourceBitmap.getWidth() / 2, (float) sourceBitmap.getHeight() / 2);
+        // Return result
+        return Bitmap.createBitmap(sourceBitmap, 0, 0, sourceBitmap.getWidth(), sourceBitmap.getHeight(), matrix, true);
     }
 
     public static Bitmap rotateBitmapOrientation(String photoFilePath) {
@@ -54,27 +73,5 @@ public class BitmapUtils {
         } else {
             return bm;
         }
-    }
-
-    private static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            // Calculate ratios of height and width to requested height and width
-            final int heightRatio = Math.round((float) height / (float) reqHeight);
-            final int widthRatio = Math.round((float) width / (float) reqWidth);
-
-            // Choose the smallest ratio as inSampleSize value, this will guarantee
-            // a final image with both dimensions larger than or equal to the
-            // requested height and width.
-            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
-        }
-
-        return inSampleSize;
     }
 }
