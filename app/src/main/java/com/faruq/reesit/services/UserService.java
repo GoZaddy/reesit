eede7f183f096c57c9a4a03fccc6041862422e93
Faruq Yusuff <faruqg@fb.com>
2022-07-26 10:49:31 -0700

    create reimbursements category for a user when they sign up

--- app/src/main/java/com/faruq/reesit/services/UserService.java
+++ app/src/main/java/com/faruq/reesit/services/UserService.java
@@ -1,5 +1,7 @@
 package com.faruq.reesit.services;
 
+import com.faruq.reesit.models.ReceiptCategory;
+import com.faruq.reesit.models.User;
 import com.parse.LogInCallback;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
@@ -7,13 +9,30 @@ import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
 public class UserService {
-    public static void signUpNewUser(String email, String password, SignUpCallback callback){
+    public interface CreateNewUserCallback {
+        void done(Exception e);
+    }
+    public static void signUpNewUser(String email, String password, CreateNewUserCallback callback){
         ParseUser user = new ParseUser();
         // Set the user's username and password, which can be obtained by a forms
         user.setEmail(email);
         user.setUsername(email);
         user.setPassword(password);
-        user.signUpInBackground(callback);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    CategoryService.createReimbursementCategoryForUser(User.fromParseUser(user), new CategoryService.AddCategoryCallback() {
+                        @Override
+                        public void done(ReceiptCategory newCategory, Exception createCategoryException) {
+                            callback.done(createCategoryException);
+                        }
+                    });
+                } else {
+                    callback.done(e);
+                }
+            }
+        });
     }
 
     public static void loginUser(String email, String password, LogInCallback callback){
